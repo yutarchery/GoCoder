2001ba4d87de69859960d031c2b2b9c4a2afcabc
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2023-04-12 14:26:36 +0900

    Update default file

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
 
+func main() {
+	/** introduce scanner */
+	var scanner = bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	/** start program */
+
+}
+
+/** inputs */
+func scanInt(scanner *bufio.Scanner) int {
+	scanner.Scan()
+	num, _ := strconv.Atoi(scanner.Text())
+	return num
+}
+
+func scanInt64(scanner *bufio.Scanner) int64 {
+	scanner.Scan()
+	num, _ := strconv.Atoi(scanner.Text())
+	return int64(num)
 }
